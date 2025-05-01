2d2055ea3e81e632f427bacf5304598297fd1b48
encodecreate <manager@encodecreate.com>
2025-05-01 12:22:54 -0500

    CheerpJ page works. Can execute a .jar on the CheerpJ page. SnapCode jars not yet working -- need non-corrupted jar files.

---
+++ Controllers/CheerpJController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SnapSaves.Controllers
+{
+    public class CheerpJController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
