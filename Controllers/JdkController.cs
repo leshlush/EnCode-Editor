6fc9259ef8e6c43039a1778dd7bd8d0b955d734e
encodecreate <manager@encodecreate.com>
2025-05-07 21:06:45 -0500

    Change SnapCode iframe's height. Push ProjectID and UserID to jdk controller.

--- Controllers/JdkController.cs
+++ Controllers/JdkController.cs
@@ -5,10 +5,12 @@ namespace SnapSaves.Controllers
     public class JdkController : Controller
     {
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult Index(string projectId, string userId)
         {
+            // Pass the projectId and userId to the view
+            ViewData["ProjectId"] = projectId;
+            ViewData["UserId"] = userId;
             return View();
         }
     }
-
 }
--- Controllers/SnapCode.cs
+++ Controllers/SnapCode.cs
@@ -2,11 +2,14 @@
 
 namespace SnapSaves.Controllers
 {
-    public class SnapCode : Controller
+    public class SnapCodeController : Controller
     {
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult Index(string projectId, string userId)
         {
+            // Pass the projectId and userId to the view
+            ViewData["ProjectId"] = projectId;
+            ViewData["UserId"] = userId;
             return View();
         }
     }
