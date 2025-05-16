93db838466a78104a623cfc5d8d82dba07327f8d
encodecreate <manager@encodecreate.com>
2025-05-15 21:05:40 -0500

    Test database access on home view.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,29 +4,24 @@ using SnapSaves.Models;
 
 namespace SnapSaves.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    using SnapSaves.Data;
+
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly AppIdentityDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(AppIdentityDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var userCount = _context.Users.Count();
+            ViewData["UserCount"] = userCount;
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+
 }
---
+++ Models/HomeModel.cs
@@ -0,0 +1,22 @@
+﻿using SnapSaves.Data;
+
+namespace SnapSaves.Models
+{
+    public class HomeModel
+    {
+        private readonly AppIdentityDbContext _context;
+
+        public int UserCount { get; set; }
+
+        public HomeModel(AppIdentityDbContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+            // Simple query: count users
+            UserCount = _context.Users.Count();
+        }
+    }
+}
